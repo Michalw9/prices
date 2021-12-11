912066d2433ac2fe7cc99d3dc687895aa39e0968
unknown <michalw95@live.com>
2021-12-11 08:56:00 +0100

    New function - making urls list

--- prices.py
+++ prices.py
@@ -1,10 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+from gzip import decompress
+from lxml import etree
+
 
 strona1 = "https://www.euro.com.pl/telewizory-led-lcd-plazmowe/sony-xr85z9jaep-8k-goole-tv-sony.bhtml"
 strona2 = "https://www.mediaexpert.pl/telewizory-i-rtv/telewizory/telewizor-lg-led-55nano883"
 strona3 = "https://mediamarkt.pl/rtv-i-telewizory/telewizor-lg-oled55a13la"
+
+
+def products_urls_list():
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36"
+    sitemap_gz = requests.get("https://www.euro.com.pl/sitemap-produkty-rtv.xml.gz", headers={"User-Agent": user_agent})
+    soup = BeautifulSoup(decompress(sitemap_gz.content), 'lxml')
+    urls = soup.findAll("url")
+    url_list=[]
+    for item in urls:
+        url_list.append(item.find("loc").text)
+    return url_list
 
 def find_price(store_page):
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36"
@@ -13,6 +27,11 @@ def find_price(store_page):
     for tag in  soup.find_all("script", {"type":"application/ld+json"}):
         parsed = json.loads(tag.text)
         if "offers" in parsed and "price" in parsed["offers"] and 'name' in parsed:
-            return [parsed['name'], parsed["offers"]["price"],parsed['gtin13']]             #[0] - Name [1] - Price [3] - EAN
+            return [parsed['name'], parsed["offers"]["price"],parsed['gtin13']]             #[0] - Name [1] - Price [2] - EAN
     return None
-print(find_price(strona1))
+
+
+
+
+for page in products_urls_list()[2122:2152]:
+    print(find_price(page))
